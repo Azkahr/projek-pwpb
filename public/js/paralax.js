e593db5a0bfafee7804c75795c1cb4a3183b9824
Muhammad Naufal Muttaqin <kurabersayap002@gmail.com>
2022-04-28 07:54:49 +0700

    menghapus parameter event di paralax

--- public/js/paralax.js
+++ public/js/paralax.js
@@ -1,5 +1,5 @@
 let bg = document.getElementById("paralax");
-window.onscroll = function (e) {
+window.onscroll = function () {
     if (window.pageYOffset < 968) {
         let i = window.pageYOffset / 2 + 5;
         bg.style.top = `${i}px`;
