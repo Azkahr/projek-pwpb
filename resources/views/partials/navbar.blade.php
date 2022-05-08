987078c0b96404effbaf0af6c75376d76bf11e73
Azka <azkahar@gmail.com>
2022-05-08 18:14:46 +0700

    membuat dropdown

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -3,9 +3,15 @@
         <img class="img-logo" src="{{ asset('img/logo 41.png') }}" alt="logo smpn 41">
     </a>
     <a href="/" style="order: 1">Home</a>
-    <a href="#" style="order: 2">Profil</a>
+    <div class="dropdown" style="order: 2">
+        <a href="#" class="dropdown-toggle" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">Profil</a>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <a href="{{ route('guru') }}" class="dropdown-item">Guru</a>
+            <a href="{{ route('ekskul') }}" class="dropdown-item">Ekstrakurikuler</a>
+        </div>
+    </div>
     <a href="#" style="order: 3">Program</a>
     <a href="{{ route('berita') }}" style="order: 5">Berita</a>
     <a href="{{ route('contact') }}" style="order: 6">Kontak</a>
-    <a href="{{ route('ekskul') }}" style="order: 7">Ekstrakurikuler</a>
+    <a href="#" style="order: 7">Pengumuman</a>
 </div>
