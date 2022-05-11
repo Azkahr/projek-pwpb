3a8fc2a20712c296095357faa7a369af94455e5a
rich0021 <kurabersayap002@gmail.com>
2022-05-11 11:57:58 +0700

    homepage sejarah singkat

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,8 +39,24 @@
             <img src="{{ asset('img/kepsek-circle.png') }}" alt="Kepala Sekolah">
         </div>
     </div>
-    <div class="container-visi-misi">
-        
+    <div class="container-sejarah">
+        <div class="iframe">
+            <iframe src="https://www.google.com/maps/embed?pb=!4v1652235846122!6m8!1m7!1sCAoSLEFGMVFpcE15R1Y0YVF4VEI2SDMxcDZkX2dINnlXeGZiTlk2ekJrdU5VenVm!2m2!1d-6.915370367182747!2d107.5899976136228!3f72.84112838032318!4f1.696136004351402!5f0.40063701368813043" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <div class="text">
+            <p>SEJARAH SINGKAT</p>
+            <p>SMPN 41 Bandung</p>
+            <div class="content">
+                <p>
+                    SMP Negeri 41 Bandung merupakan sekolah menengah pertama negeri 
+                    yang berada di Kota Bandung, Jawa Barat, Indonesia. Berlokasi di 
+                    Jalan Arjuna No.18, Ciroyom, Kec. Andir, Kota Bandung, Jawa Barat. 
+                    Masa pendidikan di SMP Negeri 41 Bandung ditempuh dalam waktu tiga 
+                    tahun pelajaran, mulai dari kelas VII hingga kelas IX, seperti pada 
+                    umumnya masa pendidikan sekolah menengah pertama di Indonesia.
+                </p>
+            </div>
+        </div>
     </div>
 @endsection
 @push('scriptBottom')
