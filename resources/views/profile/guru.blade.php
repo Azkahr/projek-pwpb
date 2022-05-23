86b157e31dae884dc48737eb6ea25fbf3cfe09f6
Azka <azkahar@gmail.com>
2022-05-23 14:50:21 +0700

    fix merge conflict

--- resources/views/profile/guru.blade.php
+++ resources/views/profile/guru.blade.php
@@ -7,10 +7,6 @@
 @include('partials/navbar')
 @section('layout-container')
     <div class="containerG">
-<<<<<<< HEAD
-=======
-        
->>>>>>> ba6a697df3f3a9625080e0a0e1a50c3492500bab
         <div class="header text-center">
             <h1>Data Guru</h1>
         </div>
